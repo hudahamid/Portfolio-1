a8eadf9784a1fcab7c20cb68922671f9e51476f5
hudahamid <huda.00091581@gmail.com>
2023-04-09 21:40:26 +0300

    project demo and codes added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,8 +26,8 @@ function Projects() {
               isBlog={false}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/AmenCHW/calm-minds-project"
+              demoLink="https://calm-minds-project.netlify.app/"
             />
           </Col>
 
@@ -37,8 +37,8 @@ function Projects() {
               isBlog={false}
               title="Bits-0f-C0de"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/hudahamid/movie-project"
+              demoLink="https://movies-api-fetch.netlify.app"
             />
           </Col>
 
@@ -48,8 +48,8 @@ function Projects() {
               isBlog={false}
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/hudahamid/Articles-ReduxToolkit"
+              demoLink="https://animated-centaur-ebe725.netlify.app/"              
             />
           </Col>
 
